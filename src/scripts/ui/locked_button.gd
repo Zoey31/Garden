748366dbc2ff318c173ca0d29cfccecdbb3bb4d9
Elfling <tomasz@kominiarczuk.com>
2025-03-26 11:18:14 +0100

    Add simple locked button

--- src/scripts/ui/locked_button.gd
+++ src/scripts/ui/locked_button.gd
@@ -3,10 +3,23 @@ class_name LockedButtonHandler
 
 @export var locked: bool = true
 @export var animator: AnimationPlayer = null
+@export var key_item: Item = null
 
 func _on_use() -> void:
 	if locked:
+		var key: Item = null
 		
+		for item: Item in Inventory.items:
+			if item.name == key_item.name:
+				key = item
+				break
+		
+		if not key:
+			return
+
+		Inventory.remove_item(key)
+		locked = false
+		animator.play("open")
 		return
 		
 	super._on_use()
